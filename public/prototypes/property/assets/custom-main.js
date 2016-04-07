31dc3ebfe807a5f2599fd988f6c6169d73cb320c
Ari Ehrmann <ari.ehrmann@gmail.com>
2016-04-07 16:04:58 -0500

    Add sliding menu

--- public/prototypes/property/assets/custom-main.js
+++ public/prototypes/property/assets/custom-main.js
@@ -1,11 +1,13 @@
-  $(document).ready(function() {
-    var closed = true;
+;(function($) {
+  var closed = true;
 
-    console.log('custom loaded');
-    $('.navicon').on('click', function(e) {
-      e.preventDefault();
-      alert('hello');
+  $('.navicon').on('click', function(e) {
+    if(closed) {
       $('.mobile-menu').slideDown(200);
-    });
+    } else {
+      $('.mobile-menu').slideUp(200);
+    }
+    closed = !closed;
   });
+})(jQuery);
 
